6812209e22f957fc496847c4d8d22649f872e6ae
Nazim Shala <shalanazim@gmail.com>
2019-12-19 00:45:18 +0100

    add sign up with API

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom'
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
 
 function SignUp() {
+    const [polls, setPolls] = useState(false);
+    const firstname = useRef();
+    const lastname = useRef();
+    const email = useRef();
+    const password = useRef();
+    const submitRegistration = async _ => {
+
+        const dataObj = {
+            firstname: firstname.current.value,
+            lastname: lastname.current.value,
+            email: email.current.value,
+            password: password.current.value
+        }
+
+        try {
+            const response = await fetch('http://localhost:8080/api/user/register', {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, *cors, same-origin
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dataObj)
+            });
+            const { data } = await response.json();
+
+            // save token somewhere
+
+            console.log(data);
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+
     return (
         <>
             <Header />
@@ -15,22 +50,21 @@ function SignUp() {
                     <div className="row justify-content-end align-items-center">
                         <form className="col-md-6 bg-white p-5 rounded">
                             <div className="form-group">
-                                <label for="userFirstname">First Name</label>
-                                <input type="email" className="form-control" id="userFirstname" name="firstname" />
+                                <label htmlFor="userFirstname">First Name</label>
+                                <input type="text" className="form-control" id="userFirstname" name="firstname" ref={firstname} />
                             </div>
                             <div className="form-group">
-                                <label for="userLastname">Last Name</label>
-                                <input type="email" className="form-control" id="userLastname" name="lastname" />
+                                <label htmlFor="userLastname">Last Name</label>
+                                <input type="text" className="form-control" id="userLastname" name="lastname" ref={lastname} />
                             </div>
                             <div className="form-group">
-                                <label for="userEmail">Email address</label>
-                                <input type="email" className="form-control" id="userEmail" name="email" />
+                                <label htmlFor="userEmail">Email address</label>
+                                <input type="email" className="form-control" id="userEmail" name="email" ref={email} />
                             </div>
                             <div className="form-group">
-                                <label for="userPassword">Password</label>
-                                <input type="password" className="form-control" id="userPassword" name="password" />
+                                <label htmlFor="userPassword">Password</label>
+                                <input type="password" className="form-control" id="userPassword" name="password" ref={password} />
                             </div>
-
 
                             <div className="d-flex">
                                 <p>
@@ -39,7 +73,10 @@ function SignUp() {
                                 </p>
                             </div>
                             <div className="d-flex justify-content-end">
-                                <button type="submit" className="btn btn-primary">Submit</button>
+                                <button type="button" className="btn btn-primary" onClick={((e) => {
+                                    e.preventDefault();
+                                    submitRegistration();
+                                })}>Submit</button>
                             </div>
                         </form>
                     </div>
