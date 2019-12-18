365dc7220d3cb240fdbe36fa11089f609cb4485f
Nazim Shala <shalanazim@gmail.com>
2019-12-18 11:05:47 +0100

    update polls list,  get polls list from API

--- src/pages/polls-list.js
+++ src/pages/polls-list.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
 
 function PollsList() {
+    const [polls, setPolls] = useState(false);
+    useEffect(() => {
+        if (!polls) {
+            (async () => {
+                try {
+                    const response = await fetch('http://localhost:8080/api/poll', {
+                        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+                        mode: 'cors', // no-cors, *cors, same-origin
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        // body: null // body data type must match "Content-Type" header
+                    });
+                    const { data } = await response.json();
+                    setPolls(data);
+                } catch (error) {
+                    console.error('Error:', error);
+                }
+            })();
+        }
+    }, [polls]);
+    console.log(polls);
+    
     return (
         <>
             <Header />
             <section className="container-fluid hero hero--register d-flex flex-grow-1 justify-content-center align-items-center">
                 <section className="container">
                     <div className="row justify-content-center align-items-center">
-                        <div class="card col-6 p-0">
-                            <div class="card-header text-center p-5">My Polls</div>
-                            <div class="card-body">
-                                <div class="list-group list-group-flush">
-                                    <Link to="/polls/detail" class="list-group-item list-group-item-action">A simple primary list group item</Link>
-                                    <Link to="/polls/detail" class="list-group-item list-group-item-action">A simple secondary list group item</Link>
-                                    <Link to="/polls/detail" class="list-group-item list-group-item-action">A simple success list group item</Link>
-                                    <Link to="/polls/detail" class="list-group-item list-group-item-action disabled"><s>A simple danger list group item</s></Link>
+                        <div className="card col-6 p-0">
+                            <div className="card-header text-center p-5">My Polls</div>
+                            <div className="card-body">
+                                <div className="list-group list-group-flush">
+                                    {polls.length && polls.map(poll => 
+                                        <Link to={'/polls/detail/'+poll._id} className="list-group-item list-group-item-action" key={poll._id}>{poll.questions}</Link>
+                                    )}
                                 </div>
                             </div>
                         </div>
