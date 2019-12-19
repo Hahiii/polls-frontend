1f0df4fa8cb27f1c5d149c5035c0c974593d66ca
Nazim Shala <shalanazim@gmail.com>
2019-12-19 22:46:37 +0100

    add success modal after voting

--- src/pages/polls-detail-user.js
+++ src/pages/polls-detail-user.js
@@ -1,11 +1,12 @@
 import React, { useRef, useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
 
 function PollsDetailUserView() {
     const [optionVal, setOptionVal] = useState('');
     const [pollDetail, setPollDetail] = useState(false);
+    const [notVoted, setNotVoted] = useState(false);
     const email = useRef();
     const validation = useRef();
     const id = window.location.search.slice(1);
@@ -48,7 +49,7 @@ function PollsDetailUserView() {
             });
 
             const { data } = await response.json()
-            console.log(data)
+            setNotVoted(data)
         } catch (error) {
 
             console.error('Error:', error);
@@ -68,42 +69,58 @@ function PollsDetailUserView() {
                     <h1 className="display-2 text-white">Vote</h1>
                 </section>
             </section>
-            <section className="container bg-white h-100">
-                <div className="row">
-                    {pollDetail && <>
-                        <div className="col-md-6 p-5">
-                            <h2 className="">{pollDetail.questions}</h2>
-                            <form className="">
-                                {pollDetail.anwser.map((anwser, i) =>
-                                    <div className="form-check p-3" key={i}>
-                                        <input className="form-check-input" type="radio" name="answers" id="exampleRadios1" value={anwser.text} onChange={(e) => setOptionVal(e.target.value)} />
-                                        <label className="form-check-label" htmlFor="exampleRadios1">{anwser.text}</label>
+            {!notVoted ? <>
+                <section className="container bg-white h-100">
+                    <div className="row">
+                        {pollDetail && <>
+                            <div className="col-md-6 p-5">
+                                <h2 className="">{pollDetail.questions}</h2>
+                                <form className="">
+                                    {pollDetail.anwser.map((anwser, i) =>
+                                        <div className="form-check p-3" key={i}>
+                                            <input className="form-check-input" type="radio" name="answers" id="exampleRadios1" value={anwser.text} onChange={(e) => setOptionVal(e.target.value)} />
+                                            <label className="form-check-label" htmlFor="exampleRadios1">{anwser.text}</label>
+                                        </div>
+                                    )}
+                                    <div className="form-group mt-4">
+                                        <label htmlFor="validation">Email</label>
+                                        <input type="email" className="form-control" id="validation" name="validation" placeholder="example@example.com" ref={email} />
                                     </div>
-                                )}
-                                <div className="form-group mt-4">
-                                    <label htmlFor="validation">Email</label>
-                                    <input type="email" className="form-control" id="validation" name="validation" placeholder="example@example.com" ref={email} />
-                                </div>
-                                <div className="form-group mt-4">
-                                    <label htmlFor="validation">Enter Validation Key</label>
-                                    <input type="text" className="form-control" id="validation" name="validation" placeholder="Secret Key" ref={validation} />
-                                </div>
-                                <p className="">{new Date().toDateString() === new Date(pollDetail.deadline).toDateString() ? `Poll is closed.` : `Poll closes on: ${new Date(pollDetail.deadline).toDateString()}`}</p>
-                                {new Date().toDateString() === new Date(pollDetail.deadline).toDateString() ? `` :
-                                    <div className="d-flex justify-content-end">
-                                        <button type="button" className="btn btn-primary" onClick={(e) => {
+                                    <div className="form-group mt-4">
+                                        <label htmlFor="validation">Enter Validation Key</label>
+                                        <input type="text" className="form-control" id="validation" name="validation" placeholder="Secret Key" ref={validation} />
+                                    </div>
+                                    <p className="">{new Date().toDateString() === new Date(pollDetail.deadline).toDateString() ? `Poll is closed.` : `Poll closes on: ${new Date(pollDetail.deadline).toDateString()}`}</p>
+                                    {new Date().toDateString() === new Date(pollDetail.deadline).toDateString() ? `` :
+                                        <div className="d-flex justify-content-end">
+                                            <button type="button" className="btn btn-primary" onClick={(e) => {
+                                                e.preventDefault();
+                                                submitVote();
+                                            }}>Submit Vote</button>
+                                        </div>
+                                    }
+                                </form>
+                            </div>
+                        </>}
+                    </div>
+                </section>
+            </> : <>
+                    <div className="vh-100 bg-light" id="exampleModal">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title text-success text-center m-4 p-5">Your Vote has been successful submited.</h5>
+                                    <button type="button" className="close">
+                                        <span onClick={(e) => {
                                             e.preventDefault();
-                                            submitVote();
-                                        }}>Submit Vote</button>
-                                    </div>
-                                }
-
-                            </form>
+                                            window.location.replace("/");
+                                        }}>&times;</span>
+                                    </button>
+                                </div>
+                            </div>
                         </div>
-                    </>}
-                </div>
-            </section>
-
+                    </div>
+                </>}
             <Footer />
         </>
     );
