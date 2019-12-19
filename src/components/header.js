132bbae8841110e2e9c65f121fdb239415f7bd8b
Nazim Shala <shalanazim@gmail.com>
2019-12-20 00:08:28 +0100

    add logout

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-
+import { ReactComponent as Logo } from '../images/poll-logo.svg'
 function Header({ admin, login, user, list, create }) {
+
+    const logOut = _ => {
+        localStorage.removeItem("id");
+        localStorage.removeItem("token");
+        window.location.replace("/")
+        return null;
+    }
+
+
     return (
         <header>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary py-3">
-                <Link className="navbar-brand" to="#">Logo</Link>
+                <Link className="navbar-brand" to="/">{<Logo className="text-light" />}</Link>
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -24,6 +33,9 @@ function Header({ admin, login, user, list, create }) {
                                     <Link className="nav-link" to="/polls/create">Create Poll</Link>
                                 </li>
                             }
+                            <li className="nav-item">
+                                <Link className="nav-link" to="#" onClick={() => logOut()}>Log out</Link>
+                            </li>
                         </>
                             :
                             <>
