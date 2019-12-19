9e19204222b017589949d6272b32f7ba126bc7bd
Nazim Shala <shalanazim@gmail.com>
2019-12-19 17:13:12 +0100

    update hearder

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-function Header({ admin, login, user }) {
+function Header({ admin, login, user, list, create }) {
     return (
         <header>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary py-3">
@@ -14,12 +14,16 @@ function Header({ admin, login, user }) {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ml-auto">
                         {admin ? <>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">My Polls</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/polls/create">Create Poll</Link>
-                            </li>
+                            {!list &&
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/">My Polls</Link>
+                                </li>
+                            }
+                            {!create &&
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/polls/create">Create Poll</Link>
+                                </li>
+                            }
                         </>
                             :
                             <>
