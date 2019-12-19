bf03d08fbdbb94517db402f4abfa0e7b624a0dbd
Nazim Shala <shalanazim@gmail.com>
2019-12-20 00:08:42 +0100

    add log in with API

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom'
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
 
 function LogIn({ loggedIn }) {
+    const email = useRef();
+    const password = useRef();
+
     if (loggedIn) {
         window.location.replace("/")
-        return;
+        return null;
     }
+
+    const submitLogIn = async _ => {
+        const dataObj = {
+            email: email.current.value,
+            password: password.current.value
+        }
+
+        try {
+            const response = await fetch('http://localhost:8080/api/user/login', {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, *cors, same-origin
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dataObj)
+            });
+            const { data } = await response.json();
+            
+            /* Save Token on local Storage */
+            localStorage.setItem('token', JSON.stringify(data.token));
+            localStorage.setItem('id', JSON.stringify(data.userId));
+            if (localStorage.getItem('id')) {
+                window.location.replace('/');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
 
     return (
         <>
@@ -23,11 +55,11 @@ function LogIn({ loggedIn }) {
                         <form className="col-md-6 bg-white p-5 rounded">
                             <div className="form-group">
                                 <label htmlFor="userEmail">Email address</label>
-                                <input type="email" className="form-control" id="userEmail" name="" />
+                                <input type="email" className="form-control" id="userEmail" name="email" ref={email} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="userPassword">Password</label>
-                                <input type="password" className="form-control" id="userPassword" name="" />
+                                <input type="password" className="form-control" id="userPassword" name="passwrod" ref={password} />
                             </div>
                             <div className="d-flex">
                                 <p>
@@ -36,7 +68,10 @@ function LogIn({ loggedIn }) {
                                 </p>
                             </div>
                             <div className="d-flex justify-content-end">
-                                <button type="submit" className="btn btn-primary">Submit</button>
+                                <button type="button" className="btn btn-primary" onClick={(e) => {
+                                    e.preventDefault();
+                                    submitLogIn();
+                                }}>Submit</button>
                             </div>
                         </form>
                     </div>
